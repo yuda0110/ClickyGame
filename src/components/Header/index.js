43ce591e81d1ee3324865cbc450308278c0bb726
Yuko Uda <yuko.uda0110@gmail.com>
2020-06-19 14:11:46 -0700

    Implementing Header components and use google fonts

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+
+const Header = props => {
+  return (
+    <header>
+      <h1>Clicky Game!</h1>
+      <p>Click on an image to earn points, but don't click on any more than once!</p>
+    </header>
+  )
+}
