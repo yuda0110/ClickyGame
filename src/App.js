498a547d0786e0f18e219b60e28d895c21f4974d
Yuko Uda <yuko.uda0110@gmail.com>
2020-06-19 17:41:18 -0700

    Create shuffleArr function for shuffling ImageCards

--- src/App.js
+++ src/App.js
@@ -17,6 +17,14 @@ class App extends Component {
     message: 'Click an image to begin!'
   }
 
+  shuffleArr = ([...array]) => {
+    for (let i = array.length - 1; i >= 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]
+    }
+    return array
+  }
+
   clickHandler = id => {
     if (this.state.clickedIds.includes(id)) {
       this.setState({
@@ -33,6 +41,9 @@ class App extends Component {
       })
     }
     console.log(this.state.clickedIds)
+    this.setState({
+      animals: this.shuffleArr(this.state.animals)
+    })
   }
 
   render() {
