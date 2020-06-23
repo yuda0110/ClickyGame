88f1be12070d8646de6d860eb912eeeecfc0ad95
Yuko Uda <yuko.uda0110@gmail.com>
2020-06-22 17:48:31 -0700

    Updates

---
+++ precache-manifest.894165cb481570df4060dc5da00ddeeb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2966cafe7a12885e4f163af9568808",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "73eaf910510edb97e733",
+    "url": "/ClickyGame/static/css/main.cf8b61d6.chunk.css"
+  },
+  {
+    "revision": "13e8429e3d86b104fb77",
+    "url": "/ClickyGame/static/js/2.73ffcd4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClickyGame/static/js/2.73ffcd4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73eaf910510edb97e733",
+    "url": "/ClickyGame/static/js/main.f5559315.chunk.js"
+  },
+  {
+    "revision": "89a095ec4bfe9c70f928",
+    "url": "/ClickyGame/static/js/runtime-main.f02c0ab2.js"
+  },
+  {
+    "revision": "eea670505e2129342594ce93c7708a3c",
+    "url": "/ClickyGame/static/media/animal-bg.eea67050.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ClickyGame/static/media/logo.5d5d9eef.svg"
+  }
+]);
